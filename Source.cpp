40a701f88f1996b3f8b0afbdcc9808226f1979bc
cecoIryna <fashionableirina2018@gmail.com>
2025-03-20 19:22:15 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <winsock2.h>
+#include <windows.h>
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+
+void SetCursorPosition(int x, int y) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD pos = { (SHORT)x, (SHORT)y };
+    SetConsoleCursorPosition(hConsole, pos);
+}
+
+int main() {
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    SOCKET server = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(server, (sockaddr*)&addr, sizeof(addr));
+    listen(server, SOMAXCONN);
+    cout << "Server is waiting for connection..." << endl;
+
+    SOCKET client = accept(server, 0, 0);
+    cout << "Client connected." << endl;
+
+    int coords[2];
+    int oldX = -1, oldY = -1;
+
+    while (true) {
+        int bytes = recv(client, (char*)coords, sizeof(coords), 0);
+        if (bytes <= 0) break;
+
+        if (oldX != -1 && oldY != -1) {
+            SetCursorPosition(oldX, oldY);
+            cout << " ";
+        }
+
+
+        SetCursorPosition(coords[0], coords[1]);
+        cout << ":)";
+
+        oldX = coords[0];
+        oldY = coords[1];
+    }
+
+    closesocket(client);
+    closesocket(server);
+    WSACleanup();
+    return 0;
+}
